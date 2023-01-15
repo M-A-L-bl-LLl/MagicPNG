e6c37f19a4fe7bd7179695a729dc40e1d7c70a4c
M-A-L-bl-LLl <lose.den16@gmail.com>
2023-01-15 16:59:21 +0300

    1st UI

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# This is a sample Python script.
-
+# This is a sample Python scrip
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from tkinter import filedialog as fd
 
 
 def print_hi(name):
@@ -9,8 +10,19 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def callback():
+    name = fd.askopenfilename()
+    print(name)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    window = Tk()
+    window.geometry('600x400')
+    window.title("MagicPNG")
+    openBtn = Button(window, text='Click to Open File', command=callback).pack(pady=10)
+    #openBtn.grid()
+    window.resizable(False, False)
+    window.mainloop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
